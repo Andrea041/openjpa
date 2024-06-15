eb7f4cd81d365f2914371542b01974b3bd8a81e5
Andrea041 <116633306+Andrea041@users.noreply.github.com>
2024-06-15 16:43:41 +0200

    Translate one comment

--- openjpa-kernel/src/test/java/org/apache/openjpa/util/testUtil/InvalidKeyValue.java
+++ openjpa-kernel/src/test/java/org/apache/openjpa/util/testUtil/InvalidKeyValue.java
@@ -3,7 +3,7 @@ package org.apache.openjpa.util.testUtil;
 public class InvalidKeyValue {
     @Override
     public int hashCode() {
-        // Genera un valore casuale, violando la consistenza
+        // Generate random value -> inconsistent object
         return (int) (Math.random() * 1000);
     }
 }
